d061c38d4d743898e73a927391168476c23bf16e
Marcin Jabłoński <nnxcomputing@gmail.com>
2023-03-23 22:38:41 +0100

    Fix passing the register context to isr_handler

--- kernel/src/includes/isr.h
+++ kernel/src/includes/isr.h
@@ -11,10 +11,10 @@ extern void isr14();
 
 typedef struct 
 {
-    uint64_t ds;
-    uint64_t rdi, rsi, rbp, rsp, rbx, rdx, rcx, rax;
-    uint64_t interrupt, error; 
-    uint64_t rip, cs, rflags, rsp_user, ss; 
+    uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
+    uint64_t rdi, rsi, rbp, rbx, rdx, rcx, rax;
+    uint64_t error, interrupt, ignore; 
+    uint64_t rip, cs, rflags, rsp, ss; 
 } __attribute__((packed)) Registers;
 
 typedef void (*ISRHandler)(Registers* regs);
